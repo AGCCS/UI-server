b7bba9a0c0a6cf25ca336762315b0fd1e1ea64e4
Vfeuer <xinhuang.gong@fau.de>
2021-05-15 00:10:23 +0200

    firmware upload added

--- component/controller/pubControl.js
+++ component/controller/pubControl.js
@@ -1,5 +1,8 @@
 var mqtt = require('mqtt')
-const {MQTT_CONF} = require('../../conf/configuration')
+const {MQTT_CONF, dirName} = require('../../conf/configuration')
+const {readInfo} = require('./subControl')
+const crc32 = require('crc32');
+var fs = require("fs");
 
 // select the phases for the node
 function setPhase (macADR, Phases) {
@@ -70,10 +73,98 @@ function noBlink (macADR) {
     })
 }
 
+function espUpload (version, board) {
+    var client = mqtt.connect(MQTT_CONF)
+    var message = JSON.stringify(
+            { "cmd": "upgrade",
+              "version": version,
+              "board": board })
+              
+    client.on('connect', function () {
+        client.publish('/DEMESH/root/control', message, {qos:1})
+        client.end()
+    })
+}
+
+function avrUpload (fileName, macADR, macAddr) {
+    const firmware = fs.readFileSync(dirName+"/"+fileName)
+    const byteLength = firmware.length
+    var avraddr = 0
+    var client = mqtt.connect(MQTT_CONF)
+    // client.subscribe(('/DEMESH/'+macADR+'/acknowledge', {qos:1}))
+    client.subscribe('/DEMESH/+/acknowledge',{qos:1})
+    var cmd = JSON.stringify(
+            { "dst": macADR,
+              "cmd": "avrota",
+              "state": "recimg"
+            })
+              
+    client.on('connect', function () {
+        client.publish('/DEMESH/root/control', cmd, {qos:1})
+    })
+    client.on('message', function (topic,message) {
+        var mesJson = JSON.parse(message)
+        if (mesJson.mtype === 'avrota') {
+            if(mesJson.state === 'recimg') {
+                let avrdata = Buffer.from(firmware.slice(avraddr, avraddr+128), 'binary')
+                let avrdataString = avrdata.toString('base64')
+                var crc = crc32(avrdata)
+                var crcNum = parseInt(crc, 16)
+                if (crcNum >= 2**31) {
+                    crcNum = crcNum - 2**32
+                }
+                var mes = JSON.stringify({
+                    "dst": macAddr,
+                    "cmd": "avrimg",
+                    "avraddr": avraddr,
+                    "avrdata": avrdataString,
+                    "avrcrc": crcNum
+                })
+                client.publish('/DEMESH/root/control', mes, {qos:1})
+            }
+            else if (mesJson.state === 'running') {
+                readInfo(macADR)
+                client.end()
+            }
+        }
+        else if (mesJson.mtype === 'avrimg' && mesJson.avrcrc==='ok') {
+            avraddr = mesJson.avraddr+128
+            if(avraddr <= byteLength) {
+                let avrdata = Buffer.from(firmware.slice(avraddr, avraddr+128), 'binary')
+                let avrdataString = avrdata.toString('base64')
+                var crc = crc32(avrdata)
+                var crcNum = parseInt(crc, 16)
+                if (crcNum >= 2**31) {
+                    crcNum = crcNum - 2**32
+                }
+                var mes = JSON.stringify({
+                    "dst": macAddr,
+                    "cmd": "avrimg",
+                    "avraddr": avraddr,
+                    "avrdata": avrdataString,
+                    "avrcrc": crcNum
+                })
+                client.publish('/DEMESH/root/control', mes, {qos:1})
+            }
+            else {
+                var mes = JSON.stringify({
+                    "dst": macAddr,
+                    "cmd": "avrota",
+                    "state": "flash",
+                    "avrimgcnt": byteLength
+                })
+                client.publish('/DEMESH/root/control', mes, {qos:1})
+            }
+        }
+    })
+}
+
 module.exports = {
     setPhase,
     pressButtonB,
     setMaxCur,
     Blink,
-    noBlink
+    noBlink,
+    espUpload,
+    avrUpload
 }
