d003574ae4857b35b6160eeb0c979104c0834efa
Vfeuer <xinhuang.gong@fau.de>
2021-05-22 23:51:20 +0200

    upload err fixed

--- component/routes/upload.js
+++ component/routes/upload.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var fs = require("fs");
+const crc32 = require('crc32');
 const { SuccessModel, ErrorModel } = require('../model/resModel')
-const {dirName} = require('../../conf/configuration')
-const {espUpload, avrUpload} = require('../controller/pubControl');
+const {readInfo} = require('../controller/subControl')
+var mqtt = require('mqtt')
+const {MQTT_CONF, dirName} = require('../../conf/configuration')
 const adminCheck = require('../midware/adminCheck');
 
 router.post('/', adminCheck, (req, res, next) => {
@@ -22,9 +24,18 @@ router.post('/', adminCheck, (req, res, next) => {
 
 router.post('/ESP32', adminCheck, (req, res, next) => {
     const board = req.body.Board
-    const version = req.body.Version
-    espUpload(version, board)
-    return res.json( new SuccessModel({'msg': 'ESP32 will download the Firmaware "'+board+'" v'+version+' now.', 'status': 202})
+    const ver = req.body.Version
+    var client = mqtt.connect(MQTT_CONF)
+    var message = JSON.stringify(
+            { "cmd": "upgrade",
+              "version": ver,
+              "board": board })
+              
+    client.on('connect', function () {
+        client.publish('/DEMESH/root/control', message, {qos:1})
+        client.end()
+    })
+    return res.json( new SuccessModel({'msg': 'ESP32 will download the Firmaware "'+board+'" v'+ver+' now.', 'status': 202})
     )
 });
 
@@ -32,7 +43,79 @@ router.post('/AVR', adminCheck, (req, res, next) => {
     const fileName = req.body.fileName
     const macADR = req.body.macADR
     const macAddr = req.body.macAdress
-    avrUpload(fileName, macADR, macAddr)
+    const firmware = fs.readFileSync(dirName+"/"+fileName)
+    const byteLength = firmware.length
+    var avraddr = 0
+    var client = mqtt.connect(MQTT_CONF)
+    // client.subscribe(('/DEMESH/'+macADR+'/acknowledge', {qos:1}))
+    client.subscribe('/DEMESH/+/acknowledge',{qos:1})
+    var cmd = JSON.stringify(
+            { "dst": macADR,
+              "cmd": "avrota",
+              "state": "recimg"
+            })
+              
+    client.on('connect', function () {
+        client.publish('/DEMESH/root/control', cmd, {qos:1})
+	console.log('cmd is: ', cmd)
+    })
+    client.on('message', function (topic,message) {
+        var mesJson = JSON.parse(message)
+	console.log('node res: ', mesJson)
+        if (mesJson.mtype === 'avrota') {
+            if(mesJson.state === 'recimg') {
+                let avrdata = Buffer.from(firmware.slice(avraddr, avraddr+128), 'binary')
+                let avrdataString = avrdata.toString('base64')
+                var crc = crc32(avrdata)
+                var crcNum = parseInt(crc, 16)
+                if (crcNum >= 2**31) {
+                    crcNum = crcNum - 2**32
+                }
+                var mes = JSON.stringify({
+                    "dst": macAddr,
+                    "cmd": "avrimg",
+                    "avraddr": avraddr,
+                    "avrdata": avrdataString,
+                    "avrcrc": crcNum
+                })
+                client.publish('/DEMESH/root/control', mes, {qos:1})
+		console.log('cmd is: ', mes)
+            }
+            else if (mesJson.state === 'running') {
+                readInfo(macADR)
+                client.end()
+            }
+        }
+        else if (mesJson.mtype === 'avrimg' && mesJson.avrcrc==='ok') {
+            avraddr = mesJson.avraddr+128
+            if(avraddr <= byteLength) {
+                let avrdata = Buffer.from(firmware.slice(avraddr, avraddr+128), 'binary')
+                let avrdataString = avrdata.toString('base64')
+                var crc = crc32(avrdata)
+                var crcNum = parseInt(crc, 16)
+                if (crcNum >= 2**31) {
+                    crcNum = crcNum - 2**32
+                }
+                var mes = JSON.stringify({
+                    "dst": macAddr,
+                    "cmd": "avrimg",
+                    "avraddr": avraddr,
+                    "avrdata": avrdataString,
+                    "avrcrc": crcNum
+                })
+                client.publish('/DEMESH/root/control', mes, {qos:1})
+            }
+            else {
+                var mes = JSON.stringify({
+                    "dst": macAddr,
+                    "cmd": "avrota",
+                    "state": "flash",
+                    "avrimgcnt": byteLength
+                })
+                client.publish('/DEMESH/root/control', mes, {qos:1})
+            }
+        }
+    })
     return res.json( new SuccessModel({'msg': 'Node ' + macADR + 'will download the Firmaware ' + fileName + 'now.', 'status': 202})
     )
 });
