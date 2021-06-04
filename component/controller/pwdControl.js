9f7bb14a3bc2aeb851f9b4e6d708188a0b6f5631
Vfeuer <76412279+Vfeuer@users.noreply.github.com>
2021-06-04 21:45:15 +0200

    using AES encryption

--- component/controller/pwdControl.js
+++ component/controller/pwdControl.js
@@ -1,18 +1,39 @@
-const crypto = require('crypto')
+const crypto = require('crypto');
 
-const SECRET_KEY = 'NJ#K$11_P[C'
+const algorithm = 'aes-192-cbc';
+const encrypPWD = 'ojb109h'
+const salt = 'nalk8n1'
 
-function md5(content) {
-    let md5 = crypto.createHash('md5')
-    return md5.update(content).digest('hex')
+const key = crypto.scryptSync(encrypPWD, salt, 24);
+const iv = Buffer.alloc(16, '1no3o5axjp95mdl2');
+
+function genPassword(password) {
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    let encrypted = '';
+    cipher.on('readable', () => {
+        let chunk;
+        while (null !== (chunk = cipher.read())) {
+            encrypted += chunk.toString('hex');}
+    });
+    cipher.write(password);
+    cipher.end();
+    return encrypted
 }
 
-// generate the secret password
-function genPassword(password) {
-    const str = `password=${password}&key=${SECRET_KEY}`
-    return md5(str)
+function pwdDecry (encryPWD) {
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    let decrypted = '';
+    decipher.on('readable', () => {
+        while (null !== (chunk = decipher.read())) {
+            decrypted += chunk.toString('utf8');
+        }
+    });
+    decipher.write(encryPWD, 'hex');
+    decipher.end();
+    return decrypted
 }
 
 module.exports = {
-    genPassword
+    genPassword,
+    pwdDecry
 }
